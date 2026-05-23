9db93dd364d3c45a58cd75fb989891e0f32ede21
Vallab007 <vallabkandi01@gmail.com>
2026-05-22 23:12:17 -0400

    Integrate PostgreSQL storage with Kafka consumer

--- consumer.py
+++ consumer.py
@@ -1,17 +1,44 @@
 from kafka import KafkaConsumer
+import json
+import psycopg2
 
 print("Starting consumer...")
-
-consumer = KafkaConsumer(
-    "user-events",
-    bootstrap_servers="localhost:9094",
-    auto_offset_reset="earliest",
-    consumer_timeout_ms=10000
-)
-
 print("Reading messages...")
 
-for message in consumer:
-    print(message.value.decode("utf-8"))
+# PostgreSQL connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="events_db",
+    user="airflow",
+    password="airflow",
+    port=5432
+)
 
-print("Done reading.")
+cursor = conn.cursor()
+
+consumer = KafkaConsumer(
+    'user-events',
+    bootstrap_servers='localhost:9094',
+    auto_offset_reset='earliest',
+    group_id='postgres-group-1',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    data = message.value
+
+    print(data)
+
+    cursor.execute(
+        """
+        INSERT INTO events (user_id, event, event_time)
+        VALUES (%s, %s, %s)
+        """,
+        (
+            data['user_id'],
+            data['event'],
+            data['timestamp']
+        )
+    )
+
+    conn.commit()
